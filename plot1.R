6b6b7546a5f3a3333ef660356001986ddc58c77f
Mark Spoto <msspoto@gmail.com>
2017-09-07 14:07:26 -0500

    Plot 1 files

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
-setwd("d:/CourseraClass/ExploratoryDataAnalysis/Week1/ExData_Plotting1")
+setwd("p:/gitprojects/exploratory-data-analysis-project1")
+
+dataCompressedFile <- "./household_power_consumption.zip"
+datafile <- "./household_power_consumption.txt"
 
 ## Load Library and download packages if not installed
 packages <- c("datasets", "dplyr")
@@ -6,8 +9,16 @@ if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
 
+library(dplyr)
+library(datasets)
+
+## decompress the data file from a zip package - NOTE: this could take some time
+if (!file.exists(datafile)) {
+  unzip(dataCompressedFile, unzip = "internal", overwrite = TRUE, exdir = ".")
+}
+
 ## Open and read the house hold power consumption file
-data <- read.csv("./household_power_consumption.txt", sep = ";", header = TRUE, fill = TRUE, na.strings = "?")
+data <- read.csv(datafile, sep = ";", header = TRUE, fill = TRUE, na.strings = "?")
 head(data)
 names(data)
 
@@ -31,6 +42,6 @@ axis(side=1, at=seq(0,6, by=2))
 axis(side=2, at=seq(0,1200, by=200))
 
 ## Save the histogram to file as PNG
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="./plot1.png", height=480, width=480)
 dev.off()
 
